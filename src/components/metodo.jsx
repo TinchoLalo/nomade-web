14190435dad71e043a8731f8d236555bb77a19c2
TinchoLalo <martinuriellaloggia@gmail.com>
2024-08-22 14:24:34 -0300

    metodo

--- src/components/metodo.jsx
+++ src/components/metodo.jsx
@@ -14,13 +14,13 @@ export function Metodo() {
                     
                     <div className="flex flex-col mmd:flex-row justify-center text-center mx-auto items-center">
                         
-                        <div className="w-full mmd:w-2/4 text-center mt-32 text-1 mx-auto">
+                        <div className="w-full mmd:w-2/4 text-center mt-20 text-1 mx-auto">
                             <h3 className="text-5xl">Metodología de <br />aprendizaje</h3>
                             <img src={logo} alt="logo" className="w-[80%] mx-auto p-10" />
                             <h4 className="text-5xl">S.T.E.A.M</h4>
                         </div>
                         
-                        <div className="w-full mmd:w-2/4 text-center mt-8 md:mt-32 text-1 mx-auto">
+                        <div className="w-full mmd:w-2/4 text-center mt-12 md:mt-32 text-1 mx-auto">
                             <div className="w-full mmd:w-[85%] h-auto rounded-2xl p-8 bg-color2 mb-8">
                                 <h5 className="text-6xl flex text-left absolute">🎲</h5>
                                 <h4 className="ml-20 block text-2xl text-left font-bold">Gamificación</h4>
